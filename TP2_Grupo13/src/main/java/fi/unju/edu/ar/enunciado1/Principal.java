d6e34c84631bd45f449d38e1fd582591d58b118b
PC <PC@Gustavo>
2022-04-17 18:41:44 -0300

    Realizacion del primer enunciado funcionando - Cari Gustavo

--- TP2_Grupo13/src/main/java/fi/unju/edu/ar/enunciado1/Principal.java
+++ TP2_Grupo13/src/main/java/fi/unju/edu/ar/enunciado1/Principal.java
@@ -1,10 +1,34 @@
 package fi.unju.edu.ar.enunciado1;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int n,r=1,aux;
+		
+		Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese el numero a factoriar: ");
+        n = teclado.nextInt();
 
+		aux=n;
+		
+		if( n <= 10 && n > 1 ){
+			for(int i=1;i<=aux;i++){
+				r = r * n;
+				n --;
+			}
+			System.out.println("Resultado : " + r);
+		}
+		else {
+			if( n==0 || n==1 ) {
+				r = 1;
+				System.out.println("Resultado : " + r);
+			}
+			else {
+				System.out.println("Valor ingresado esta fuera de los limites.");
+			}
+		}
 	}
-
 }
