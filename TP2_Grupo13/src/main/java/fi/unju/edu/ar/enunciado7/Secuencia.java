dadc40b8e68949671d2673cbe4c2f40bac37f251
Familia Díaz <Familia Daz@DESKTOP-K0V8RO8>
2022-04-17 21:11:10 -0300

    Enunciado7 secuencia1

---
+++ TP2_Grupo13/src/main/java/fi/unju/edu/ar/enunciado7/Secuencia.java
@@ -0,0 +1,16 @@
+package fi.unju.edu.ar.enunciado7;
+
+public class Secuencia {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int j=41;
+        int b=2;
+        for(int i=39;i>=-39;i=i-b)
+        {
+            j=j + i;
+            System.out.println(j);
+        }
+    }
+
+}
