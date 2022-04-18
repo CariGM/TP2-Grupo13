0d244d0ab39f2b7dcfc68e2f961eb3a1287a2b2d
Familia Díaz <Familia Daz@DESKTOP-K0V8RO8>
2022-04-18 00:10:29 -0300

    Punto16 matematica

--- TP2_Grupo13/src/main/java/fi/unju/edu/ar/enunciado16/Principal.java
+++ TP2_Grupo13/src/main/java/fi/unju/edu/ar/enunciado16/Principal.java
@@ -1,27 +1,28 @@
 package fi.unju.edu.ar.enunciado16;
 
-
 import java.util.Scanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import fi.unju.edu.ar.enunciado16.CalculoMatematico;
+
 @SpringBootApplication
 public class Principal {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Principal.class, args);
-        Scanner teclado = new Scanner(System.in);
-
-        CalculoMatematico dev = new CalculoMatematico();
-
-        System.out.println("Ingrese un valor numerico:");
-        int num = teclado.nextInt();
-
-        System.out.println("El resultado de la sumatoria es: " + dev.calcularSumatoria(num));
-
-        System.out.println("El resultado del producto es: " + dev.calcularProductoria(num));
-
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Principal.class, args);
+		Scanner teclado = new Scanner(System.in);
+		
+		CalculoMatematico dev = new CalculoMatematico();
+		
+		System.out.println("Ingrese un valor numerico:");
+		int num = teclado.nextInt();
+		
+		System.out.println("El resultado de la sumatoria es: " + dev.calcularSumatoria(num));
+		
+		System.out.println("El resultado del producto es: " + dev.calcularProductoria(num));
+		
+	}
 
 }
