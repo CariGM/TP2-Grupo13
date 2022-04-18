ffa9b3e4889c3a18c493a25f806d6c8c4e626840
PC <PC@DESKTOP-TVIBC5F>
2022-04-17 21:34:29 -0300

    Secuencia While Gaspar Gonzalo

---
+++ TP2_Grupo13/src/main/java/fi/unju/edu/ar/enunciado10/SecuenciaWhile.java
@@ -0,0 +1,17 @@
+package fi.unju.edu.ar.enunciado10;
+
+import java.util.Scanner;
+
+public class SecuenciaWhile {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int num=320;
+        while(num>160)
+        {
+            num=num-20;
+            System.out.println(num);
+        }
+    }
+
+}
